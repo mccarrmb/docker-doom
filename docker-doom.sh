fd6e53b0472682f338f10f7ffd1facd4d4e30d68
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-02-22 11:50:41 -0500

    added player dir to launcher

--- docker-doom.sh
+++ docker-doom.sh
@@ -3,5 +3,6 @@ docker run \
   -v ./iwad:/home/zandronum/iwad \
   -v ./wad:/home/zandronum/wad \
   -v ./config/:/home/zandronum/config \
+  -v ./player/:/home/zandronum/player \
   -p 10666:10666 \
   -d docker-doom
