7cc74ae99d352f5bb9cc2c21722ce4b0d2ec2e4f
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-02-23 09:26:41 -0500

    forgot that single quotes meant literal

--- build-docker-doom.sh
+++ build-docker-doom.sh
@@ -3,7 +3,7 @@
 if [ "`command -v docker`" = '' ]; then
   echo "You have to install docker before you can build the container."
   exit 1
-elif [ '$#' -gt 1 ]; then
+elif [ "$#" -gt 1 ]; then
    docker build . -t $1
 else
   docker build . -t docker-doom
