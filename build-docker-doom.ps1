8d7d0dbee4c139c6a19c79986e89bc593306df61
mccarrmb <mccarrmb@gmail.com>
2021-10-31 01:53:34 -0400

    Fixed for regular windows hosts

--- build-docker-doom.ps1
+++ build-docker-doom.ps1
@@ -1,30 +1,42 @@
 param(
-    [Parameter(HelpMessage="Build the Windows version of the docker-doom container instead")]
-    [switch]$Windows = $False
+    [string]
+    # Specifies the running container name
+    $Name = 'docker-doom',
+
+    [string]
+    # Specifies the container host OS (linux, windows, or windows-server)
+    $HostOs = 'linux'
 )
+
+<#
+        .SYNOPSIS
+        Creates a Doom multiplayer server container
+
+        .DESCRIPTION
+        Creates a Doom multiplayer server container for either Linux, Windows,
+        or Windows Server based hosts.
+#>
 
 # This is needed to keep PoSh from displaying that ugly red error
 $CurrentPreference = $ErrorActionPreference
 $ErrorActionPreference = 'stop'
 
-$image_tag = "docker-doom"
-
-# Set a custom tag name if desired
-if ($args.Length -gt 0) { $image_tag = args[0] } 
+$SupportedOs = 'linux','windows','windows-server'
 
 # Remove the previously used Dockerfile in case the linux build was executed
 if (Test-Path .\Dockerfile) { Remove-Item .\Dockerfile }
 
-# Allows Docker to build a windows-based container
-if ($Windows) {
-    Copy-Item ".\windows\Dockerfile" ".\"
-} else {
-    Copy-Item ".\linux\Dockerfile" ".\"
+if ($SupportedOs.Contains($HostOs)) {
+    Copy-Item ".\$HostOs\Dockerfile" ".\"
+}
+else {
+    Write-Error "The -HostOs `"$HostOs`" is not supported"
+    Exit
 }
 
 try { 
     if (Get-Command docker) {
-        docker build . -t "$image_tag"
+        docker build . -t "$Name"
     } 
 }
 catch { "You have to install docker before you can build the container." }
